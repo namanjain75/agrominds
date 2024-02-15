a36dfcf8092b0e460c5bd8cf1ecb1e6c71fb87ed
Naman Jain <namanjain7567@gmail.com>
2024-02-16 00:06:44 +0530

    About us section updated and login page updated

--- app.js
+++ app.js
@@ -13,25 +13,23 @@ app.get("/",(req,res)=>{
 })
 
 app.get("/disease_detection",(req,res)=>{
-    res.render("disease_detection.ejs")
+    res.render("disease_detection.ejs");
 })
 
 app.get("/pricing",(req,res)=>{
-    res.render("pricing.ejs")
+    res.render("pricing.ejs");
 })
 
 app.get("/aboutUs",(req,res)=>{
-    res.render("aboutUs.ejs") 
+    res.render("aboutUs.ejs") ;
 })
 
 app.get("/login",(req,res)=>{
-    res.render("login.ejs")
+    res.render("login.ejs");
    
 })
 
-app.get("/register",(req,res)=>{
-    res.send("regestration page"); 
-})
+
 
 app.listen(port,()=>{
     console.log(`The server is started on port ${port}`);
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,70 +1,17 @@
-
-(function ($) {
-    "use strict";
-
-
-    /*==================================================================
-    [ Focus Contact2 ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
-                $(this).addClass('has-val');
-            }
-            else {
-                $(this).removeClass('has-val');
-            }
-        })    
-    })
-  
-  
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
-
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
-
-        return check;
-    });
-
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-
-})(jQuery);
+const loginText = document.querySelector(".title-text .login");
+      const loginForm = document.querySelector("form.login");
+      const loginBtn = document.querySelector("label.login");
+      const signupBtn = document.querySelector("label.signup");
+      const signupLink = document.querySelector("form .signup-link a");
+      signupBtn.onclick = (()=>{
+        loginForm.style.marginLeft = "-50%";
+        loginText.style.marginLeft = "-50%";
+      });
+      loginBtn.onclick = (()=>{
+        loginForm.style.marginLeft = "0%";
+        loginText.style.marginLeft = "0%";
+      });
+      signupLink.onclick = (()=>{
+        signupBtn.click();
+        return false;
+      });
